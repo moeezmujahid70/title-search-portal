4a1c25d5dcac4b88a4b135776827d53303ae421d
Moeez Mujahid <moeezmujahid70@gmail.com>
2025-07-13 01:21:44 +0200

    added user field

--- search_portal/core/models.py
+++ search_portal/core/models.py
@@ -1,20 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Certificate(models.Model):
     cert_number = models.CharField(max_length=100, unique=True)
     county = models.CharField(max_length=100)
-
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='certificates')
     STATUS_CHOICES = [
         ('in_process', 'In Process'),
         ('ready', 'Ready'),
     ]
     status = models.CharField(
-        max_length=20, choices=STATUS_CHOICES, default='in_process')
-
-    created_at = models.DateTimeField(
-        auto_now_add=True)  # Set once when created
-    updated_at = models.DateTimeField(auto_now=True)      # Updated every save(
+        max_length=30, choices=STATUS_CHOICES, default='in_process')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.cert_number} - {self.county}"
+        return f"{self.cert_number} - {self.county} ({self.user.username})"
