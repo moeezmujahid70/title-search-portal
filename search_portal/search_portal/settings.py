059c30f01c50b4aa01b9cc92962ddfd5bda696b0
Moeez Mujahid <moeezmujahid70@gmail.com>
2025-07-18 16:20:24 +0200

    switching database

--- search_portal/search_portal/settings.py
+++ search_portal/search_portal/settings.py
@@ -126,18 +126,18 @@ WSGI_APPLICATION = 'search_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',      # SQLite backend
-#         # Database file at BASE_DIR/db.sqlite3
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('PORTAL_DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',      # SQLite backend
+        # Database file at BASE_DIR/db.sqlite3
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.environ.get('PORTAL_DATABASE_URL'))
+# }
 
 
 REST_FRAMEWORK = {
